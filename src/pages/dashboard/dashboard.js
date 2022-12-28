a836b7026501d2e33b0ed5f790bde0656164aa45
Yaqoob <sualehfarooq65@gmail.com>
2022-12-28 18:52:17 +0500

    Update dashboard
    commit

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -4,6 +4,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { Drawer } from "../../components/drawer/drawer";
 import { Button, Card } from "react-bootstrap";
+import ProgressBar from "react-bootstrap/ProgressBar";
 export default function Dashboard() {
   return (
     <>
@@ -15,40 +16,46 @@ export default function Dashboard() {
           <Col className="dash-data">
             {/* Dashboad Upper */}
             <Row className="dash-top">
-              <div style={{flex : 1}} >
+              <div style={{ flex: 1 }}>
                 <p>Welcome Back</p>
                 <h3>Project Dashboad</h3>
               </div>
               <div className="dash-top-btn">
                 <Button className="btn btn-light projectBtn ">Download</Button>
-                <Button className="btn btn-dark projectBtn">Generate Report</Button>
+                <Button className="btn btn-dark projectBtn">
+                  Generate Report
+                </Button>
               </div>
             </Row>
             {/* Dashboad Cards */}
-            <Row className="sales-card" >
-<div className="server-div">
-    <Card className="card_">
-        <Card.Title className="card-title">Server
-        <p className="percent">62%</p> 
+            <Row className="sales-card">
+              <div className="server-div">
+                <Card className="card_">
+                  <Card.Title className="card-title">
+                    Server
+                    <p className="percent">62%</p>
+                  </Card.Title>
+                  <Card.Body>
+                    <p style={{ fontSize: "small" }}>
+                      6% higher than last month
+                    </p>
+                    <hr className="line"></hr>
+                    <ProgressBar  now={60} />
 
-        </Card.Title> 
-        <Card.Body>
-        <p style={{fontSize:'small'}}>6% higher than last month</p> 
-        <hr className="line" ></hr>
-        </Card.Body>
-    </Card>
-    <Card className="card_">
-        <Card.Title className="card-title">Server</Card.Title>
-    </Card>
-</div>
-<div className="graph-div" >
-<Card className="gp-card">
-        <Card.Title className="card-title">Server</Card.Title>
-    </Card>
-    <Card className="gp-card">
-        <Card.Title className="card-title">Server</Card.Title>
-    </Card>
-</div>
+                  </Card.Body>
+                </Card>
+                <Card className="card_">
+                  <Card.Title className="card-title">Server</Card.Title>
+                </Card>
+              </div>
+              <div className="graph-div">
+                <Card className="gp-card">
+                  <Card.Title className="card-title">Server</Card.Title>
+                </Card>
+                <Card className="gp-card">
+                  <Card.Title className="card-title">Server</Card.Title>
+                </Card>
+              </div>
             </Row>
           </Col>
         </Row>
